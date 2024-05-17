c69463d80440a4b88a7255add0b236c573990f81
zakaria jaddad <111153070+zakaria-jaddad@users.noreply.github.com>
2024-05-17 17:00:28 +0100

    Refactor: Fixed UnFound sounds sources

--- src/components/Settings/data/sounds/alarmSoudns.js
+++ src/components/Settings/data/sounds/alarmSoudns.js
@@ -1,43 +1,43 @@
 const alarmSounds = [
   {
     alarmName: "piano",
-    alarmSound: "./sounds/piano.mp3",
+    alarmSound: "/sounds/piano.mp3",
   },
   {
     alarmName: "scarey",
-    alarmSound: "./sounds/scarey.mp3",
+    alarmSound: "/sounds/scarey.mp3",
   },
   {
     alarmName: "twitch",
-    alarmSound: "./sounds/twitch.mp3",
+    alarmSound: "/sounds/twitch.mp3",
   },
   {
     alarmName: "victory",
-    alarmSound: "./sounds/victory-1.mp3",
+    alarmSound: "/sounds/victory-1.mp3",
   },
   {
     alarmName: "duck",
-    alarmSound: "./sounds/quack.mp3",
+    alarmSound: "/sounds/quack.mp3",
   },
   {
     alarmName: "lofi",
-    alarmSound: "./sounds/lofi-1.mp3",
+    alarmSound: "/sounds/lofi-1.mp3",
   },
   {
     alarmName: "lofi",
-    alarmSound: "./sounds/lofi-2.mp3",
+    alarmSound: "/sounds/lofi-2.mp3",
   },
   {
     alarmName: "did it",
-    alarmSound: "./sounds/victory-2.mp3",
+    alarmSound: "/sounds/victory-2.mp3",
   },
   {
     alarmName: "stop",
-    alarmSound: "./sounds/time-to-stop.mp3",
+    alarmSound: "/sounds/time-to-stop.mp3",
   },
   {
     alarmName: "drums",
-    alarmSound: "./sounds/drums-1.mp3",
+    alarmSound: "/sounds/drums-1.mp3",
   },
 ];
 
